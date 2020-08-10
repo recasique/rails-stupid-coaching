c38496684a5ad5a597f503491aeef78029e9aa10
Rafael Casique <recasique@gmail.com>
2020-08-10 15:17:55 +0200

    lego

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,12 @@
+class QuestionsController < ApplicationController
+  def answer
+    @question = params[:answer]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
+end
